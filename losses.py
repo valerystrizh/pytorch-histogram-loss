eaf51c24fc420e665314488915f3caf535ab2c62
Valeriya <32123957+valerystrizh@users.noreply.github.com>
2018-02-07 19:45:33 +0300

    minor changes

--- losses.py
+++ losses.py
@@ -2,13 +2,13 @@ import torch
 from torch.autograd import Variable
 
 class HistogramLoss(torch.nn.Module):
-    def __init__(self, num_steps, use_gpu=True):
+    def __init__(self, num_steps, cuda=True):
         super(HistogramLoss, self).__init__()
         self.step = 2 / (num_steps - 1)
-        self.use_gpu = use_gpu
+        self.cuda = cuda
         self.t = torch.range(-1, 1, self.step).view(-1, 1)
         self.tsize = self.t.size()[0]
-        if self.use_gpu:
+        if self.cuda:
             self.t = self.t.cuda()
         
     def forward(self, features, classes):
@@ -26,7 +26,7 @@ class HistogramLoss(torch.nn.Module):
         classes_eq = (classes.repeat(classes_size, 1)  == classes.view(-1, 1).repeat(1, classes_size)).data
         dists = torch.mm(features, features.transpose(0, 1))
         s_inds = torch.triu(torch.ones(classes_eq.size()), 1).byte()
-        if self.use_gpu:
+        if self.cuda:
             s_inds= s_inds.cuda()
         pos_inds = classes_eq[s_inds].repeat(self.tsize, 1)
         neg_inds = ~classes_eq[s_inds].repeat(self.tsize, 1)
@@ -40,7 +40,7 @@ class HistogramLoss(torch.nn.Module):
         histogram_neg = histogram(neg_inds, neg_size)
         histogram_pos_repeat = histogram_pos.view(-1, 1).repeat(1, histogram_pos.size()[0])
         histogram_pos_inds = torch.tril(torch.ones(histogram_pos_repeat.size()), -1).byte()
-        if self.use_gpu:
+        if self.cuda:
             histogram_pos_inds = histogram_pos_inds.cuda()
         histogram_pos_repeat[histogram_pos_inds] = 0
         histogram_pos_cdf = histogram_pos_repeat.sum(0)
