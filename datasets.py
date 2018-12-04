d7934756914b2c8f297b06b91b7de1b0cf2747a1
Valeriya <32123957+valerystrizh@users.noreply.github.com>
2018-12-04 20:39:25 +0300

    merge to one class

--- datasets.py
+++ datasets.py
@@ -5,32 +5,24 @@ from PIL import Image
 from sklearn.preprocessing import LabelEncoder
 from torch.utils.data import Dataset
 
-class ImageDatasetTest(Dataset):
-    def __init__(self, paths, transform):
+class ImageDataset(Dataset):
+    def __init__(self, paths, transform, is_train, labels=None):
         self.img_paths = np.array(paths)
         self.transform = transform
+        self.is_train = is_train
+        if self.is_train:
+            self.labels = LabelEncoder().fit_transform(labels)
+            self.labels = torch.from_numpy(np.array(self.labels, dtype=np.float))
 
     def __getitem__(self, index):
         img_path = self.img_paths[index]
         img = self.transform(Image.open(img_path))
-        return img
+        if self.is_train:
+            label = self.labels[index]
+            return img, label
+        else:
+            return img
         
-    def __len__(self):
-        return len(self.img_paths)
-    
-class ImageDatasetTrain(Dataset):
-    def __init__(self, paths, labels, transform):
-        self.img_paths = np.array(paths)
-        self.labels = LabelEncoder().fit_transform(labels)
-        self.labels = torch.from_numpy(np.array(self.labels, dtype=np.float))
-        self.transform = transform
-    
-    def __getitem__(self, index):
-        img_path = self.img_paths[index]
-        img = self.transform(Image.open(img_path))
-        label = self.labels[index]
-        return img, label
-    
     def __len__(self):
         return len(self.img_paths)
     
